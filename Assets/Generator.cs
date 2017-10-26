cf5316f2858eb333a349e11add2c7acaee012c9d
Hidekazu Noji <nkc.hidekazu.noji@gmail.com>
2017-10-26 10:58:43 +0900

    見えないボールを飛ばす途中

---
+++ Assets/Generator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+    public GameObject Sphere;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+
+        }
+	}
+}
---
+++ Assets/ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ball : MonoBehaviour
+{
+    public GameObject Ball; 
+public void Shoot(Vector3 dir)
+{
+        GetComponent<Rigidbody>().AddForce(dir);
+}
+ void Start()
+    {
+    //    Shoot(new Vector3(0, 200, 2000));     
+
+    }
+     void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject Ball = Instantiate(BallP) as GameObject;
+
+            Ray ray = camera.main.scrwwnpointToRay(Input.mousePosition);
+            Ball.GetComponent<ball>().Shoot(worldDir.normalized * 2000);
+        }
+    }
+}
